7a83acaacc04a981e09701e695b0782be069eaf8
Kejie-Wang <wang_kejie@foxmail.com>
2018-03-05 16:09:49 +0800

    update dockfile

--- docker/scripts/docker_create.sh
+++ docker/scripts/docker_create.sh
@@ -5,6 +5,4 @@ SHELL_PATH="$(cd "$( dirname "$0"  )" && pwd  )"
 DOCKER_REPO="kejiewang/develop-environment"
 TAG="latest"
 
-
-
 docker build -t "${DOCKER_REPO}:${TAG}" -f "$SHELL_PATH/../Dockfile.gpu" $SHELL_PATH/../..
